cabab19151be1635e16b08721c0081d8d633836f
Lasiya <52373359+SLdevilX@users.noreply.github.com>
2021-04-12 23:40:42 +0530

    Update social-scraper.js

--- plugins/social-scraper.js
+++ plugins/social-scraper.js
@@ -93,7 +93,7 @@ if (cn.WORKTYPE == 'private') {
             })
 
             await message.sendMessage(Buffer.from(profileBuffer.data), MessageType.video, {
-              caption: 'Made by WhatsAsena',
+              caption: 'Made by X-Troid',
             })
           })
           .catch(
@@ -167,7 +167,7 @@ else if (cn.WORKTYPE == 'public') {
             })
 
             await message.sendMessage(Buffer.from(profileBuffer.data), MessageType.video, {
-              caption: 'Made by WhatsAsena',
+              caption: 'Made by X-Troid',
             })
           })
           .catch(
