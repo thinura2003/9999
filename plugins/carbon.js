3343d4194fe981e0fe983f64aac92178802977f8
Lasiya <52373359+SLdevilX@users.noreply.github.com>
2021-04-14 17:32:50 +0530

    Update carbon.js

--- plugins/carbon.js
+++ plugins/carbon.js
@@ -1,6 +1,9 @@
-/* Codded by @phaticusthiccy
-Telegram: t.me/phaticusthiccy
-Instagram: www.instagram.com/kyrie.baran
+/* # Exclusively from danuma project 
+# Do not use this fore any commercial thing
+# If you abuse thais bot we wil kick you from bot 
+# Do not edit (Respect to the Devaoloper) 
+# All rights reserved ©Lasiya @lasiya99X t.me/lasiya99X
+# Get more about devaoloper https://lasiya.ml
 */
 
 const Asena = require('../events');
