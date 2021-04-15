3f0a97f393101ae8aed4b71c1c16e44a96299151
Lasiya <52373359+SLdevilX@users.noreply.github.com>
2021-04-15 14:20:23 +0530

    Update plugin.js

--- plugins/sql/plugin.js
+++ plugins/sql/plugin.js
@@ -1,9 +1,9 @@
-/* Copyright (C) 2020 Yusuf Usta.
-
-Licensed under the  GPL-3.0 License;
-you may not use this file except in compliance with the License.
-
-WhatsAsena - Yusuf Usta
+/* # Exclusively from danuma project 
+# Do not use this fore any commercial thing
+# If you abuse thais bot we wil kick you from bot 
+# Do not edit (Respect to the Devaoloper) 
+# All rights reserved ©Lasiya @lasiya99X t.me/lasiya99X
+# Get more about devaoloper https://lasiya.ml
 */
 
 const config = require('../../config');
