ee9bd6a338b4237e652a0b1599ad70c3b9126ef7
Lasiya <52373359+SLdevilX@users.noreply.github.com>
2021-04-25 09:56:05 +0530

    Update ttp.js

--- plugins/ttp.js
+++ plugins/ttp.js
@@ -111,7 +111,7 @@ else if (Config.WORKTYPE == 'public') {
 
     }));
 
-    Asena.addCommand({ pattern: 'glowttp ?(.*)', fromMe: false, desc: Lang.GLOW_DESC }, (async (message, match) => {
+    Asena.addCommand({ pattern: 'glow ?(.*)', fromMe: false, desc: Lang.GLOW_DESC }, (async (message, match) => {
 
         if (match[1] === '') return await message.sendMessage(Lang.NEED_WORD);
 
