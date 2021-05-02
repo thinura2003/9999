06aef23067156621b376889c762404b11dfb9390
Lasiya <52373359+SLdevilX@users.noreply.github.com>
2021-05-02 18:14:29 +0530

    Update test.js

--- plugins/test.js
+++ plugins/test.js
@@ -13,7 +13,5 @@ Asena.addCommand({pattern: 'test', fromMe: true, deleteCommand: false,}, (async 
         r_text[1] = "https://telegra.ph/file/eae11a10fe0d71b59e8ef.jpg";
 
 var respoimage = await axios.get(`${r_text[1]}`, { responseType: 'arraybuffer' })
-
-await message.client.sendMessage(message.jid, Buffer.from(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: 'Made by X-Troid'})
-
+await message.client.sendMessage(message.jid, Buffer.from(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: '    *Quinn lเzค* \n    *Privert Assistant*\n\n*Hosted*  • ๛ ```Aws```\n*Version*  • ๛ ```v1.8 Global Stable```\n*Branch*  • ๛ ```Master```\n*Dev*  • ๛ ```t.me/lasi99X```\n*Dev.About*  • ๛ ```lasiya.ml```\n*Moderator*  • ๛ ```@SLPlaygames_Owner```\n\n*Main Group*\nhttps://t.me/danuma01'})
     }));
