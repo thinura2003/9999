dcd9460d5facc8c5a85eec34635461fa371632ad
Lasiya <52373359+SLdevilX@users.noreply.github.com>
2021-05-02 20:05:26 +0530

    Update config.js

--- config.js
+++ config.js
@@ -19,7 +19,7 @@ DATABASE_URL = process.env.DATABASE_URL === undefined ? './whatsasena.db' : proc
 DEBUG = process.env.DEBUG === undefined ? false : convertToBool(process.env.DEBUG);
 
 module.exports = {
-    VERSION: 'v1.0 Global Stable',
+    VERSION: 'v1.8 Global',
     SESSION: process.env.ASENA_SESSION === undefined ? '' : process.env.ASENA_SESSION,
     GANSTYLE: process.env.GAN_IMAGE === undefined ? 'https://telegra.ph/file/9298dc3611d056b85b63b.jpg' : process.env.GAN_IMAGE,
     LANG: process.env.LANGUAGE === undefined ? 'TR' : process.env.LANGUAGE.toUpperCase(),
