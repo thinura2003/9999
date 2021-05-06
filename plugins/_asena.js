c16d094d19b0eb1092588a984c71a40ff9c2a8ac
Lasiya <52373359+SLdevilX@users.noreply.github.com>
2021-05-06 18:17:49 +0530

    Update _asena.js

--- plugins/_asena.js
+++ plugins/_asena.js
@@ -44,7 +44,7 @@ if (Config.WORKTYPE == 'private') {
             );
         
             await message.client.sendMessage(
-                message.jid,'╔════════════════════╗\n  ●▬▬ *X-Troid PVT  Bot* ▬▬●\n\n' + CMD_HELP, MessageType.text
+                message.jid,'╔════════════════════╗\n  ✥▬▬ *X-Troid PVT  Bot* ▬▬✥\n\n' + CMD_HELP, MessageType.text
             );    
         } else {
 
@@ -76,7 +76,7 @@ if (Config.WORKTYPE == 'private') {
             );
             if (CMD_HELP === '') CMD_HELP += Lang.NOT_FOUND;
             await message.client.sendMessage(
-                message.jid,'╔════════════════════════════╗\n  ●▬▬ *X-Troid PVT  Bot* ▬▬●\n\n' + CMD_HELP, MessageType.text
+                message.jid,'╔════════════════════════════╗\n  ✥▬▬ *X-Troid PVT  Bot* ▬▬✥\n\n' + CMD_HELP, MessageType.text
             );
         }
     }));
