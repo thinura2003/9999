44506d5d9bddf7762cabef98ec9fe62ac6e87ee0
Lasiya <52373359+SLdevilX@users.noreply.github.com>
2021-05-14 11:29:27 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -484,7 +484,7 @@ ${chalk.blue.italic('ℹ️ Connecting to WhatsApp... Please Wait.')}`);
                     let sendMsg = false;
                     var chat = conn.chats.get(msg.key.remoteJid)
                         
-                    if ((config.SUDO == '94762234883' && msg.key.fromMe === false && command.fromMe === false &&
+                    if ((config.SUDO !== false && msg.key.fromMe === false && command.fromMe === true &&
                         (msg.participant && config.SUDO.includes(',') ? config.SUDO.split(',').includes(msg.participant.split('@')[0]) : msg.participant.split('@')[0] == config.SUDO || config.SUDO.includes(',') ? config.SUDO.split(',').includes(msg.key.remoteJid.split('@')[0]) : msg.key.remoteJid.split('@')[0] == config.SUDO)
                     ) || command.fromMe === msg.key.fromMe || (command.fromMe === false && !msg.key.fromMe)) {
                         if (command.onlyPinned && chat.pin === undefined) return;
