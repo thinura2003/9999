13f09c204883effb1fd713e652f0e38bf16540d6
Lasiya <52373359+SLdevilX@users.noreply.github.com>
2021-05-14 23:49:43 +0530

    Update someonesay.js

--- plugins/someonesay.js
+++ plugins/someonesay.js
@@ -27,15 +27,6 @@ if (Config.WORKTYPE == 'private') {
 
     }));
     
-     Asena.addCommand({ pattern: 'tes ?(.*)', fromMe: true, desc: Lang.ANIME_DESC }, (async (message, match) => {
-
-        if (match[1] === '') return await message.sendMessage(Lang.NEED_WORD);
-
-        var ttinullimage = await axios.get(`https://nekobot.xyz/api/imagegen?type=iphonex&text=${match[1].replace(/Ö/g, "%C3%96").replace(/ö/g, "%C3%B6").replace(/ü/g, "%C3%BC").replace(/Ü/g, "%C3%9C").replace(/Ğ/g, "%C4%9E").replace(/ğ/g, "%C4%9F").replace(/ş/g, "%C5%9F").replace(/Ş/g, "%C5%9E").replace(/ç/g, "%C3%A7").replace(/Ç/g, "%C3%87").replace(/ı/g, "%C4%B1").replace(/i/g, "%69").replace(/"/g, "%22").replace(/İ/g, "%C4%B0")}&raw=1`, { responseType: 'arraybuffer' })
-
-        await message.client.sendMessage(message.jid,Buffer.from(ttinullimage.data), MessageType.image, { mimetype: Mimetype.png, caption: 'Made by X-Troid🛸' })
-
-    }));
 
     Asena.addCommand({ pattern: 'changesay ?(.*)', fromMe: true, desc: Lang.CHANGE_DESC }, (async (message, match) => {
 
