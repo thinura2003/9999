c42071886dcef19ceeab6fd390150e98f7af92f3
Lasiya <52373359+SLdevilX@users.noreply.github.com>
2021-05-28 14:36:27 +0530

    Update ocr.js

--- plugins/ocr.js
+++ plugins/ocr.js
@@ -5,7 +5,6 @@
 # All rights reserved ©Lasiya @lasiya99X t.me/lasiya99X
 # Get more about devaoloper https://lasiya.ml
 */
-
 const Asena = require('../events');
 const Config = require('../config');
 const {MessageType} = require('@adiwajshing/baileys');
@@ -54,6 +53,40 @@ if (Config.WORKTYPE == 'private') {
 else if (Config.WORKTYPE == 'public') {
 
     Asena.addCommand({pattern: 'ocr ?(.*)', fromMe: false, desc: Lang.OCR_DESC}, (async (message, match) => { 
+
+        if (message.reply_message === false) return await message.sendMessage(Lang.NEED_REPLY);    
+	var info = await message.reply(Lang.DOWNLOADING);
+        var location = await message.client.downloadAndSaveMediaMessage({
+            key: {
+                remoteJid: message.reply_message.jid,
+                id: message.reply_message.id
+            },
+            message: message.reply_message.data.quotedMessage
+        });
+
+        var dil;
+        if (match[1] !== '') {
+            dil = langs.where("1", match[1]);
+        } else {
+            dil = langs.where("1", Config.LANG.toLowerCase());
+        }
+
+        try {
+            var result = await tesseract.recognize(location, {
+                lang: dil[2]
+            });    
+        } catch (e) {
+            return await message.reply(Lang.ERROR.format(e));
+        }
+
+        await info.delete();
+        if ( result === ' ' || result.length == 1 ) {
+            return await message.reply(Lang.ERROR.format(' Empty text'));
+        }
+
+        return await message.reply(Lang.RESULT.format(dil[2], result));
+    }));
+    Asena.addCommand({pattern: 'ocr ?(.*)', fromMe: true, desc: Lang.OCR_DESC, dontAddCommandList: true}, (async (message, match) => { 
 
         if (message.reply_message === false) return await message.sendMessage(Lang.NEED_REPLY);    
 	var info = await message.reply(Lang.DOWNLOADING);
