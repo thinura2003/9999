86e49996492f434f1ed9741efed70be6f7feb39c
Lasiya <52373359+SLdevilX@users.noreply.github.com>
2021-05-29 00:29:20 +0530

    Create ttppornhub.js

---
+++ plugins/ttppornhub.js
@@ -0,0 +1,43 @@
+/* # Exclusively from danuma project 
+# Do not use this fore any commercial thing
+# If you abuse thais bot we wil kick you from bot 
+# Do not edit (Respect to the Devaoloper) 
+# All rights reserved Â©Lasiya @lasiya99X t.me/lasiya99X
+# Get more about devaoloper https://lasiya.ml
+*/
+
+const Asena = require('../events');
+const { MessageType, MessageOptions, Mimetype } = require('@adiwajshing/baileys');
+const fs = require('fs');
+const axios = require('axios');
+const Config = require('../config');
+
+const Language = require('../language');
+const Lang = Language.getString('ttp');
+
+
+if (Config.WORKTYPE == 'private') {
+
+    Asena.addCommand({ pattern: 'phub ?(.*)', fromMe: true,}, (async (message, match) => {
+
+        if (match[1] === '') return await message.sendMessage(Lang.NEED_WORD);
+
+        var ttinullimage = await axios.get(`https://api.lolhuman.xyz/api/textprome2/pornhub?apikey=264702c251ae9c86e4673dab&text1=${encodeURIComponent(match[1])}&text2=${encodeURIComponent(match[2])}`, { responseType: 'arraybuffer' })
+
+        await message.sendMessage(Buffer.from(ttinullimage.data), MessageType.image, { mimetype: Mimetype.jpg, caption: '🚀Made by X-Troid ☄️' })
+
+    }));
+}
+
+else if (Config.WORKTYPE == 'public') {
+
+    Asena.addCommand({ pattern: 'phub ?(.*)', fromMe: false,}, (async (message, match) => {
+
+        if (match[1] === '') return await message.sendMessage(Lang.NEED_WORD);
+
+        var ttinullimage = await axios.get(`https://api.lolhuman.xyz/api/textprome2/pornhub?apikey=264702c251ae9c86e4673dab&text1=${encodeURIComponent(match[1])}&text2=${encodeURIComponent(match[2])}`, { responseType: 'arraybuffer' })
+
+        await message.sendMessage(Buffer.from(ttinullimage.data), MessageType.image, { mimetype: Mimetype.jpg, caption: '🚀Made by X-Troid ☄️' })
+
+    }));
+}
