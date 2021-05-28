6512148e32310de060e45bdd053badc1a2bafb98
Lasiya <52373359+SLdevilX@users.noreply.github.com>
2021-05-29 01:43:27 +0530

    Update ttpbt.js

--- plugins/ttpbt.js
+++ plugins/ttpbt.js
@@ -16,7 +16,7 @@ const Config = require('../config');
 
 if (Config.WORKTYPE == 'private') {
 
-    Asena.addCommand({ pattern: 'tbt ?(.*)', fromMe: true, deleteCommand: false, }, (async (message, match) => {
+    Asena.addCommand({ pattern: 'zbt ?(.*)', fromMe: true, deleteCommand: false, }, (async (message, match) => {
 
         if (match[1] === '') return await message.sendMessage();
 
@@ -29,7 +29,7 @@ if (Config.WORKTYPE == 'private') {
 
 else if (Config.WORKTYPE == 'public') {
 
-    Asena.addCommand({ pattern: 'tbt ?(.*)', fromMe: false,deleteCommand: false, }, (async (message, match) => {
+    Asena.addCommand({ pattern: 'zbt ?(.*)', fromMe: false,deleteCommand: false, }, (async (message, match) => {
 
         if (match[1] === '') return await message.sendMessage();
 
