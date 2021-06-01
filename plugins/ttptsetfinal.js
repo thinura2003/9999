94b8db75aec2d5837bd2056f78bfcfc355804776
Lasiya <52373359+SLdevilX@users.noreply.github.com>
2021-06-02 01:20:55 +0530

    Create ttptsetfinal.js

---
+++ plugins/ttptsetfinal.js
@@ -0,0 +1,48 @@
+
+/* # Exclusively from danuma project 
+# Do not use this fore any commercial thing
+# If you abuse thais bot we wil kick you from bot 
+# Do not edit (Respect to the Devaoloper) 
+# All rights reserved ©Lasiya @lasiya99X t.me/lasiya99X
+# Get more about devaoloper https://lasiya.ml
+*/
+
+const Asena = require('../events');
+const {MessageType,Mimetype} = require('@adiwajshing/baileys');
+const translatte = require('translatte');
+const config = require('../config');
+const Glang = Language.getString('github');
+
+
+
+
+
+Asena.addCommand({ pattern: 'lasi ?(.*)', fromMe: true, }, async (message, match) => {
+
+    const userName = match[1]
+
+    if (userName === '') return await message.client.sendMessage(message.jid, Glang.REPLY, MessageType.text)
+
+    await axios
+      .get(`https://ttpxc.herokuapp.com/api/textmaker?text=${userName}`)
+      .then(async (response) => {
+
+        const {
+            results,
+        } = response.data.result
+
+        const githubscrap = await axios.get(results, 
+          {responseType: 'arraybuffer',
+        })
+
+        const msg = `hello`
+
+        await message.sendMessage(Buffer.from(githubscrap.data), MessageType.image, { 
+          caption: msg,
+        })
+      })
+      .catch(
+        async (err) => await message.client.sendMessage(message.jid, Glang.NOT, MessageType.text),
+      )
+  },
+)
