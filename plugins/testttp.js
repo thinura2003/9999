e5aefb072e08b67c68c258eb6fca857f37243e3c
Lasiya <52373359+SLdevilX@users.noreply.github.com>
2021-06-01 18:05:56 +0530

    Create testttp.js

---
+++ plugins/testttp.js
@@ -0,0 +1,26 @@
+const Asena = require('../events');
+const { MessageType, MessageOptions, Mimetype } = require('@adiwajshing/baileys');
+const fs = require('fs');
+const axios = require('axios');
+const Config = require('../config');
+
+const Language = require('../language');
+const Lang = Language.getString('ttp');
+
+
+
+
+
+
+Asena.addCommand({ pattern: 'zsky ?(.*)', fromMe: true, desc: Lang.SKY_DESC }, (async (message, match) => {
+
+    if (match[1] === '') {
+        try{
+            const respo = await got(`https://ttpxc.herokuapp.com/api/textmaker?text=${match[1]}`).then(async ok  => {
+                resp = JSON.parse(ok.body);
+
+    var ttinullimage = await axios.get({resp.results}, { responseType: 'arraybuffer' })
+
+    await message.sendMessage(Buffer.from(ttinullimage.data), MessageType.image, { mimetype: Mimetype.jpg, caption: '🚀Made by X-Troid ☄️' })
+
+}));
