f5cebb10006296f54e0729f76ecfb6264c23eccf
Lasiya <52373359+SLdevilX@users.noreply.github.com>
2021-06-02 22:21:13 +0530

    Update blood.js

--- plugins/blood.js
+++ plugins/blood.js
@@ -17,7 +17,7 @@ const desk = "blood photo with your name"
 
 if (Config.WORKTYPE == 'private') {
 
-    Asena.addCommand({ pattern: 'blood ?(.*)', fromMe: true, desc: desk }, (async (message, match) => {
+    Asena.addCommand({ pattern: 'zblood ?(.*)', fromMe: true, desc: desk }, (async (message, match) => {
 
         if (match[1] === '') return await message.sendMessage();
 
@@ -30,7 +30,7 @@ if (Config.WORKTYPE == 'private') {
 
 else if (Config.WORKTYPE == 'public') {
 
-    Asena.addCommand({ pattern: 'blood ?(.*)', fromMe: false, desc: desk}, (async (message, match) => {
+    Asena.addCommand({ pattern: 'zblood ?(.*)', fromMe: false, desc: desk}, (async (message, match) => {
 
         if (match[1] === '') return await message.sendMessage();
 
