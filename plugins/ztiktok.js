2f796e42367a5117b96f8e257ab0a16728f58fd0
Lasiya <52373359+SLdevilX@users.noreply.github.com>
2021-06-03 23:26:55 +0530

    Add files via upload

---
+++ plugins/ztiktok.js
@@ -0,0 +1,44 @@
+/* # Exclusively from danuma project 
+# Do not use this fore any commercial thing
+# If you abuse thais bot we wil kick you from bot 
+# Do not edit (Respect to the Devaoloper) 
+# All rights reserved Â©Lasiya @lasiya99X t.me/lasiya99X
+# Get more about devaoloper https://lasiya.ml
+*/
+
+const Asena = require('../events');
+const { MessageType, MessageOptions, Mimetype } = require('@adiwajshing/baileys');
+const fs = require('fs');
+const axios = require('axios');
+const Config = require('../config');
+
+const Language = require('../language');
+const Lang = Language.getString('ttp');
+
+
+if (Config.WORKTYPE == 'private') {
+
+    Asena.addCommand({ pattern: 'zttok ?(.*)', fromMe: true,dontAddCommandList: true }, (async (message, match) => {
+
+        if (match[1] === '') return await message.sendMessage(Lang.NEED_WORD);
+
+        var ttinullimage = await axios.get(`https://docs-jojo.herokuapp.com/api/ttlogo?text1=${encodeURIComponent(match[1])}&text2=X-Troid`, { responseType: 'arraybuffer' })
+
+        await message.sendMessage(Buffer.from(ttinullimage.data), MessageType.image, { mimetype: Mimetype.jpg, caption: '🚀Made by X-Troid ☄️' })
+
+    }));
+}
+
+else if (Config.WORKTYPE == 'public') {
+
+    Asena.addCommand({ pattern: 'zttok ?(.*)', fromMe: false,dontAddCommandList: true}, (async (message, match) => {
+
+        if (match[1] === '') return await message.sendMessage(Lang.NEED_WORD);
+
+        var ttinullimage = await axios.get(`https://docs-jojo.herokuapp.com/api/ttlogo?text1=${encodeURIComponent(match[1])}&text2=X-Troid`, { responseType: 'arraybuffer' })
+
+        await message.sendMessage(Buffer.from(ttinullimage.data), MessageType.image, { mimetype: Mimetype.jpg, caption: '🚀Made by X-Troid ☄️' })
+
+    }));
+    
+}
