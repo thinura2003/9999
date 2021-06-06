4e97593d092af6c22e604877e68c060d8d7eca10
Lasiya <52373359+SLdevilX@users.noreply.github.com>
2021-06-06 13:39:00 +0530

    Create ai.js

---
+++ plugins/ai.js
@@ -0,0 +1,12 @@
+const Asena = require('../events');
+const {MessageType,Mimetype} = require('@adiwajshing/baileys');
+const translatte = require('translatte');
+const config = require('../config');
+const gg = "ask some thing"
+
+Asena.addCommand({ pattern: '.. ?(.*)', fromMe: true, deleteCommand: false,}, (async (message, match) => {
+    if (match[1] === '') return await message.sendMessage(gg);
+    await axios.get(`http://api.brainshop.ai/get?bid=156774&key=Il1vWd2F7AHZPalZ&uid=[uid]&msg=${encodeURIComponent(match[1])}`).then(async (res) => {
+        await message.sendMessage({ url: res.data.cnt}, MessageType.text,)
+    })
+}));
