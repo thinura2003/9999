2284110099ccd15926f9ef9113704022a9b950f7
Lasiya <52373359+SLdevilX@users.noreply.github.com>
2021-06-06 16:08:55 +0530

    Update ai.js

--- plugins/ai.js
+++ plugins/ai.js
@@ -3,12 +3,12 @@ const Asena = require('../events');
 const { MessageType } = require('@adiwajshing/baileys');
 const got = require('got');
 
-Asena.addCommand({ pattern: '. ?(.*)', fromMe: true,  }, (async (message, match) => {
+Asena.addCommand({ pattern: 'liza ?(.*)', fromMe: false,  }, (async (message, match) => {
 	if (match[1] === '') return await message.client.sendMessage(message.jid, '```Ask some thing.```', MessageType.text, { quoted: message.data });
 	let url = `http://api.brainshop.ai/get?bid=156774&key=Il1vWd2F7AHZPalZ&uid=[uid]&msg=${match[1]}`
 	const response = await got(url);
 	const json = JSON.parse(response.body);
 	let msg = '```';
-	msg += 'jj' + json.cnt + '```';
+	msg += '' + json.cnt + '```';
 	await message.client.sendMessage(message.jid, msg, MessageType.text, { quoted: message.data });
 }));
